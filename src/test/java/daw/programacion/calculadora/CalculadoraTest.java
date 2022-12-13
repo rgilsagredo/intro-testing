300ff12ad9930003aaea1f447557e53d4a526d80
Raul Gil de Sagredo <rgilsagredo@gmail.com>
2022-12-13 12:14:14 +0100

    añado los imports de JUnit

---
+++ src/test/java/daw/programacion/calculadora/CalculadoraTest.java
@@ -0,0 +1,11 @@
+package daw.programacion.calculadora;
+
+/*
+ * Necesitaremos los siguientes imports del paquete JUNIT
+ */
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CalculadoraTest {
+    
+}

b77e4af441601d169ccf4157690ac75433ee356f
Raul Gil de Sagredo <rgilsagredo@gmail.com>
2022-12-13 12:23:38 +0100

    añado un test para Calculadora.suma(). Testing: 2+2=4

--- src/test/java/daw/programacion/calculadora/CalculadoraTest.java
+++ src/test/java/daw/programacion/calculadora/CalculadoraTest.java
@@ -18,4 +18,25 @@ public class CalculadoraTest {
         assertTrue(true);
     }
 
+    // trar crear el test, decimos a MAVEN que corra los tests
+
+    /**
+     * Los test deben checkear una única funcionalidad, y deben tener nombres claros de lo
+     * que están haciendo. De momento solo tengo el método suma, así que creo un test
+     * (reality-check) para ver que 2+2=4
+     */
+
+     @Test
+     public void dosSumaDosEsCuatro(){
+        // tengo que instanciar la clase
+        Calculadora calc = new Calculadora();
+        // este es el resultado esperado
+        int target = 4;
+        // estos son los imput
+        int numero1 = 2, numero2 = 2;
+        // que lo compruebe
+        assertEquals(target,calc.suma(numero1, numero2));
+     }
+
+
 } // CalculadoraTest

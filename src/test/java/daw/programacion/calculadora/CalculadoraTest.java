cac0b05d7aa5d1e2f7af767330affeff05b2894f
Raul Gil de Sagredo <rgilsagredo@gmail.com>
2022-12-13 12:16:41 +0100

    añado un test trivial para checkear funcionalidad

--- src/test/java/daw/programacion/calculadora/CalculadoraTest.java
+++ src/test/java/daw/programacion/calculadora/CalculadoraTest.java
@@ -7,5 +7,15 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CalculadoraTest {
-    
-}
+
+    /**
+     * Un test es un método que lleva el decorador @Test
+     */
+
+    // este sería un tests de prueba
+    @Test
+    public void testTest() {
+        assertTrue(true);
+    }
+
+} // CalculadoraTest
